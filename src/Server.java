b9c054565f9a797e5f3e9f91f36ef32f468ac0f9
SimoneLeveghi <simone.leveghi@buonarroti.tn.it>
2024-11-20 14:05:45 +0100

    Implement Server ClientHandler

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,42 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    ServerSocket server;
+    Socket client;
+    final int porta;
+    public Server() {
+        server = null;
+        client = null;
+        porta = 1234;
+    }
+    private class ClientHandler implements Runnable {
+        private Socket clientSocket;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+        @Override
+        public void run () {
+            try {
+                while ( true ) {
+                    BufferedReader inDalClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    DataOutputStream outVersoClient = new DataOutputStream (clientSocket.getOutputStream());
+
+                    String message = inDalClient.readLine () ;
+                    System.out.println("Messaggio ricevuto da client : " + message);
+                    outVersoClient.writeBytes (message + "\n");
+                    outVersoClient.flush() ;
+                    }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
