5d8e45e9fdaa2b00d949eb4d75017900d575f40b
SimoneLeveghi <simone.leveghi@buonarroti.tn.it>
2024-11-20 14:35:44 +0100

    Implement Client connection

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,64 @@
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
+    private String serverHostname;
+    private int porta;
+
+    private Socket clientSocket;
+    private BufferedReader userInput;
+    private BufferedReader serverInput;
+    private DataOutputStream clientOutput;
+    private String userInputString;
+
+    public Client() {
+        serverHostname = "localhost";
+        porta = 1234;
+    }
+
+    public void connetti() {
+        System.out.println("Client in esecuzione");
+        try {
+            userInput = new BufferedReader(new InputStreamReader(System.in));
+            clientSocket = new Socket(serverHostname, porta);
+            clientOutput = new DataOutputStream(clientSocket.getOutputStream());
+            serverInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.out.println("Host sconosciuto.");
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante la connessione.");
+            System.exit(1);
+        }
+    }
+
+    public void comunica() {
+        try {
+            boolean cond = true;
+            while(!userInputString.equals("Fine")) {
+                System.out.print("Stringa da inviare al server: ");
+                userInputString = userInput.readLine();
+
+                clientOutput.writeBytes(userInputString + '\n');
+                System.out.println("Risposta dal server: " + serverInput.readLine());
+            }
+            System.out.println("Connessione terminata.");
+            clientSocket.close();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante la connessione.");
+            System.exit(1);
+        }
+    }
+
 
     public static void main(String[] args) {
-
+        Client client = new Client();
+        client.connetti();
+        client.comunica();
     }
 }
